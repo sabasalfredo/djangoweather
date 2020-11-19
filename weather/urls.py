0f7dad67491ae1f51293428c930522e57279e703
Sabasalfredo Mejia <sabasalfredo@gmail.com>
2020-11-18 22:33:29 -0800

    initial_2 commit weather

---
+++ weather/urls.py
@@ -0,0 +1,12 @@
+"""weather URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('lookup.urls')),
+]
