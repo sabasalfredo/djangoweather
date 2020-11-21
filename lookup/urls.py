c34c5a71a38659963bd7638b2fc4027de5793623
Sabasalfredo Mejia <sabasalfredo@gmail.com>
2020-11-20 23:31:00 -0800

    added api to views and home files

--- lookup/urls.py
+++ lookup/urls.py
@@ -1,3 +1,5 @@
+# This is my urls.py file
+
 from django.urls import path
 from . import views
 
--- lookup/views.py
+++ lookup/views.py
@@ -1,10 +1,18 @@
 # This is my views.py file
-
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	 return render(request, 'home.html', {})
+	import json
+	import requests
+	api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=ABDF5AE6-987F-450F-86F0-3D4C82335F1F")
+
+	try:
+		api = json.loads(api_request.content)
+	except Exception as e:
+		api = "Error..."
+
+	return render(request, 'home.html', {'api': api})
 
 def about(request):
-	 return render(request, 'about.html', {})
+	return render(request, 'about.html', {})
